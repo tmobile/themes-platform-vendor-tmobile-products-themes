3876dcb3b99b48787955687dacb210453353ec90
Josh Guilfoyle <Josh.Guilfoyle@T-Mobile.com>
2010-11-19 09:23:13 -0800

    Added Androidian and cleared default theme.
    The default theme is a legacy feature no longer being used.  Instead, we
    treat the special "system" theme as the platform default.

--- themes-base.mk
+++ themes-base.mk
@@ -1,11 +1,12 @@
 PRODUCT_PACKAGES += \
 	ThemeManager \
 	ThemeChooser \
-	com.tmobile.themes
+	com.tmobile.themes \
+	Androidian
 
 PRODUCT_PROPERTY_OVERRIDES += \
-	default_theme.package_name=com.tmobile.theme.Grenade \
-	default_theme.style_id=Grenade
+	default_theme.package_name= \
+	default_theme.style_id=
 
 PRODUCT_COPY_FILES := \
 	frameworks/base/data/etc/android.hardware.camera.xml:/system/etc/permissions/android.hardware.camera.flash.xml
