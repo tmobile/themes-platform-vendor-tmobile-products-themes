671cd64ee2d932c5836942c9cde9bcc70d9d9f25
Josh Guilfoyle <Josh.Guilfoyle@T-Mobile.com>
2011-01-27 18:27:50 -0800

    Localize mdpi,hdpi build config to just theme packages.
    Discovered a new method for compiling themes with both and m and hdpi
    assets without affecting the entire build product (see corresponding
    changes in theme packages).

--- themes-base.mk
+++ themes-base.mk
@@ -7,7 +7,3 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES := \
 	frameworks/base/data/etc/com.tmobile.software.themes.xml:/system/etc/permissions/com.tmobile.software.themes.xml
-
-# We need these to ensure that the theme packages themselves are built in a
-# "generic" fashion and can be taken to any device.
-PRODUCT_LOCALES := mdpi hdpi
